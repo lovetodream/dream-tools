2bc2a2f21c1bec360b41f7d065babb92e1bc9e41
Timo <38291523+lovetodream@users.noreply.github.com>
2023-07-08 10:55:34 +0200

    feat: add throwing public convertible

--- Sources/DreamToolsCore/PublicConvertible.swift
+++ Sources/DreamToolsCore/PublicConvertible.swift
@@ -1,6 +1,6 @@
 /// It converts a non public model to a public representation of itself.
 public protocol PublicConvertible {
-    associatedtype Public
+    associatedtype Public: Codable
 
     /// The method used to convert a non public model to a public representation of itself.
     /// It will also be available on Collections of the former.
@@ -10,5 +10,17 @@ public protocol PublicConvertible {
 public extension Collection where Element: PublicConvertible {
     func convertToPublic() -> [Element.Public] {
         return self.map { $0.convertToPublic() }
+    }
+}
+
+public protocol ThrowingPublicConvertible {
+    associatedtype Public: Codable
+
+    func convertToPublic() throws -> Public
+}
+
+public extension Collection where Element: ThrowingPublicConvertible {
+    func convertToPublic() throws -> [Element.Public] {
+        return try self.map { try $0.convertToPublic() }
     }
 }
